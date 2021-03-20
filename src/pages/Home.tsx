cda34062597a3734ad8473c836068fe075ceec57
Kato <kubkate@gmail.com>
2021-03-20 08:11:35 -0700

    reivse Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>this is home page</h1>
+    <div className="mb-6 text-gray-600">
+      <div className="container mx-auto flex px-5 pt-24 md:flex-row">
+        <h1>this is home page</h1>
+      </div>
     </div>
   )
 }
