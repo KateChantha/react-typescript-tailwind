475b5f2878de1f9e1f03febfca9dcba00ea860d4
Kato <kubkate@gmail.com>
2021-03-20 08:29:08 -0700

    add 3 pages About Team and Project

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,25 @@
-import {Route, Switch} from 'react-router-dom';
-import FrontLayout from './components/layouts/FrontLayout';
-import Home from './pages/Home';
+import {Route, Switch} from 'react-router-dom'
+import FrontLayout from './components/layouts/FrontLayout'
+import About from './pages/about/About'
+import Home from './pages/Home'
+import Project from './pages/project/Project'
+import Team from './pages/team/Team'
 
 const AppRoute = ({component: Component, layout: Layout, ...rest}: any) => (
-  <Route {...rest} render={ props => (
-   <Layout><Component {...props}></Component></Layout> 
-  )}></Route>
+   <Route {...rest} render={ props => (
+        <Layout><Component {...props}></Component></Layout> 
+   )}></Route>
 )
 
 const Routes = () => {
-  return (
-    <Switch>
-      <AppRoute path="/" excact={true} layout={FrontLayout} component={Home} />
-    </Switch>
-  )
+    return (
+        <Switch>
+            <AppRoute path="/" exact={true} layout={FrontLayout} component={Home} />
+            <AppRoute path="/about" layout={FrontLayout} component={About} />
+            <AppRoute path="/teams" layout={FrontLayout} component={Team} />
+            <AppRoute path="/projects" layout={FrontLayout} component={Project} />
+        </Switch>
+    )
 }
 
 export default Routes
