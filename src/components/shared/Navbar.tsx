51bc32262c357b3453940f5f7bed93d13064dcf2
Kato <kubkate@gmail.com>
2021-03-20 14:43:59 -0700

    Implement open/close hamberger navbar on mobile screen

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,11 +3,13 @@
 import {useState, useRef} from 'react'
 import { NavLink } from 'react-router-dom'
 import useOutsideClick from './UseOutsideClick';
-import UseOutsideClick from './UseOutsideClick'
+
 
 const Navbar = () => {
   // open/close "More" menu dropbox
   const [isActive, setIsActive] = useState(false);
+  // toggle navbar on mobile screen
+  const [isOpen, setIsOpen] = useState(false)
 
   // Event Handler, when click outside menu
   const ref = useRef<HTMLDivElement>(null);
@@ -28,21 +30,23 @@ const Navbar = () => {
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={() => setIsOpen(!isOpen)}
               >
 
               <span className="sr-only">Open main menu</span>
 
-              {/* Menu open: "hidden", Menu closed: "block" */}
-              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              {/* Mobile Hamberger & Cross */}
+              { 
+                isOpen ?
+                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              :
+                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-
-              {/* Menu open: "block", Menu closed: "hidden" */}
-              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              </svg>
-
-
+                </svg>
+              }
+              
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -242,7 +246,7 @@ const Navbar = () => {
       </div>
 
           {/* เมนูโหมด Mobile */}
-          <div className=" sm:hidden block" id="mobile-menu">
+          <div className={`${isOpen ? "block": "hidden"} sm:hidden`} id="mobile-menu">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
               <NavLink exact to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" >
@@ -257,11 +261,21 @@ const Navbar = () => {
               <NavLink exact to="/projects" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" >
                 Projects
               </NavLink>
-              <button type="button" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 inline-flex rounded-md text-sm font-medium" aria-expanded="false">
+              <button  type="button" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 inline-flex rounded-md text-sm font-medium" aria-expanded="false" onClick={() =>{
+                console.log("click")
+                setIsActive(!isActive)
+              }}>
                   <span>More</span>
-                  <svg className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
-                  </svg>
+              { 
+                isActive ?
+                <svg className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+                </svg>
+                :
+                <svg className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+              }
                   
                 </button>
             </div>
